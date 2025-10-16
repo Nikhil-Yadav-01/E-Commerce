b6465411b8b1bb454dbaad1026b422fdc6467ba9
Nikhil Yadav <nikhildevr.01@gmail.com>
2025-10-16 23:09:41 +0530

    updated categories

--- lib/features/shop/screens/home/widgets/categories.dart
+++ lib/features/shop/screens/home/widgets/categories.dart
@@ -5,6 +5,7 @@ import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/images.dart';
 import '../../../../../utils/helpers/helper_functions.dart';
 import '../../../../../utils/device/device_utility.dart';
+import '../../category/category_screen.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
@@ -20,13 +21,33 @@ class Categories extends StatelessWidget {
         itemCount: 8,
         scrollDirection: Axis.horizontal,
         itemBuilder: (_, index) {
+          final categories = [
+            {'name': 'Cosmetics', 'image': RImages.cosmeticsIcon},
+            {'name': 'Electronics', 'image': RImages.electronicsIcon},
+            {'name': 'Clothing', 'image': RImages.clothIcon},
+            {'name': 'Sports', 'image': RImages.sportIcon},
+            {'name': 'Furniture', 'image': RImages.furnitureIcon},
+            {'name': 'Shoes', 'image': RImages.shoeIcon},
+            {'name': 'Jewelry', 'image': RImages.jeweleryIcon},
+            {'name': 'Animals', 'image': RImages.animalIcon},
+          ];
+          
+          final category = categories[index % categories.length];
+          
           return VerticalImageText(
-            image: RImages.cosmeticsIcon,
-            title: 'Cosmetics',
+            image: category['image'] as String,
+            title: category['name'] as String,
             backgroundColor: isDark ? RColors.secondaryDark : RColors.secondaryLight,
             textColor: isDark ? RColors.onSecondaryDark : RColors.onSecondaryLight,
             itemSize: RDeviceUtils.getCategoryItemSize(context),
-            onTap: () {},
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => CategoryScreen(
+                  categoryName: category['name'] as String,
+                ),
+              ),
+            ),
           );
         },
       ),
