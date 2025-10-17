842d61f8f18fd3b8743d8f33708251fd45dcd085
Nikhil Yadav <nikhildevr.01@gmail.com>
2025-10-17 18:43:39 +0530

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
+  // Todo: Add Widgets Bindings
+  // Todo: Init Local Storage
+  // Todo: Init Payment Methods
+  // Todo: Await Native Splash
+  // Todo: Initialize Authentication
   runApp(const App());
 }
