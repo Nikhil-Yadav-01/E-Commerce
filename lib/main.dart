9cd3320b3dc95ea5eae20354725aa3718f6a0aa9
Nikhil Yadav <nikhildevr.01@gmail.com>
2025-10-17 19:19:58 +0530

    firebase app auth registered

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   // Todo: Add Widgets Bindings
   // Todo: Init Local Storage
   // Todo: Init Payment Methods
   // Todo: Await Native Splash
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,).then(
+      (FirebaseApp value) => Get.put(AuthenticationRepository());
+  );
   // Todo: Initialize Authentication
   runApp(const App());
 }
